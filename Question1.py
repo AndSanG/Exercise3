2bd73757a4c0e2c9b5438283249ec538f60a8fef
Andres Sanchez <and_sg@outlook.com>
2019-01-03 23:22:08 +0100

    draft working with comments

--- Question1.py
+++ Question1.py
@@ -1,29 +1,32 @@
 from Sheet import Sheet
 
-sheet = Sheet(5,5)
-#sheet.updateValue(0, A, string)
-sheet.updateValue(0, 0, '7')  #A1
-sheet.updateValue(1, 0, '5')  #A2
-sheet.updateValue(0, 1, '=(3+4)*2') #B1
-sheet.updateValue(0, 2, '=A1+B1')  #C1
-sheet.updateValue(1, 2, '=A1-B1')  #C2
-sheet.updateValue(2, 2, '=A1*B1')  #C3
-sheet.updateValue(0, 3, '=(A1+8)/2')  #D1
-sheet.updateValue(1, 3, '=max(A1,A2)') #D2
+sheet = Sheet(10,10)
+#sheet.modifyValue(0, A, string)
+'''
+sheet.modifyValue(0, 0, '7')  #A1
+sheet.modifyValue(1, 0, '5')  #A2
+sheet.modifyValue(0, 1, '=(3+4)*2') #B1
+sheet.modifyValue(0, 2, '=A1+B1')  #C1
+sheet.modifyValue(1, 2, '=A1-B1')  #C2
+sheet.modifyValue(2, 2, '=A1*B1')  #C3
+sheet.modifyValue(0, 3, '=(A1+8)/2')  #D1
+sheet.modifyValue(1, 3, '=max(A1,A2)') #D2
+'''
+#sheet.modifyValue(2, 26, "=11") #AA3
+#sheet.modifyValue(1, 0, "=AA3") #A2
+#sheet.modifyValue(0, 27, "111") #AB1
 
+#sheet.modifyValue(3, 0, '=sum([A1,B10,C10,D10])') #A4
+#sheet.modifyValue(4, 0, '=average([A1,B1,C1,D9])') #A4
 
-#sheet.updateValue(1, 0, "=AA1") #A2
-#sheet.updateValue(0, 27, "111") #A2
-sheet.updateValue(3, 0, '=A1') #A4
+sheet.modifyValue(0, 1, '7')    #B1
+sheet.modifyValue(1, 0, '=B1')  #A2
+sheet.modifyValue(1, 1, '=B1')  #B2
+sheet.modifyValue(2, 2, '=A2')  #C3
+sheet.modifyValue(0, 0, '=B2+C3')  #A1
+sheet.modifyValue(0, 1, '8')    #B1
 
+#sheet.modifyValue(4, 0, '5')  #A5
+
+#sheet.modifyValue(0, 2, '=B2')  #C1
 print(sheet)
-
-def colNameToInt(name):
-    result = 0;
-    for i in range(len(name),-1,1):
-        print(i)
-        print((ord(name[i])-65) * pow(26,i))
-    print(result)
-    return ord(name[0]) - 65
-
-colNameToInt('AA')
--- Sheet.py
+++ Sheet.py
@@ -15,17 +15,50 @@ class NumberCell():
 
 class FormulaCell():
     def __init__(self, expr, sheet):  # expr is of the shape "=A1+B2" in string
+
         self._sheet = sheet
         self.formula = expr
+        self._value = None
+        self.dependencies = []
+        self.create_dependencies_tree()
+        #self.updateValue()
+
+    def get_value(self):
         self.updateValue()
+        return self._value
+
+    def set_value(self, value):
+        self._value = value
+
+    value = property(get_value, set_value)
+
+    def input_matcher(self,input):
+        p = re.compile('[A-Z]+[1-9][0-9]?|100+')
+        return p.finditer(input)  # list of matches
+
+
+
+    def create_dependencies_tree(self):
+        input = self.formula[1:]
+        matches = self.input_matcher(input)
+
+        for match in matches:
+            self.dependencies.append(input[match.start():match.end()])  # cell number e.g. A1
 
     def updateValue(self):
-
 
         def average(aList):
             return sum(aList) / len(aList)
 
+        ### before calc the value the dependencies must be solved
+        for cell_id in self.dependencies:
+            cell = self._sheet.lookupCell(cell_id)
+            if type(cell) is FormulaCell:
+                cell.updateValue()
+
         # Eval the content of cell (without =) and save in the value
+        #addCall convert A1 into lookup(A1)
+        #lookup search the value
         self.value = eval(self.addCalls(self.formula[1:]))
 
 
@@ -33,8 +66,7 @@ class FormulaCell():
     # so A1 => self.lookup('A1')
     # A1 + B1 => self.lookup('A1') + self.lookup('B1')
     def addCalls(self, input):
-        p = re.compile('[A-Z]+[1-9][0-9]?|100+')
-        matches = p.finditer(input)                             #list of matches
+        matches = self.input_matcher(input)                     #list of matches
         result = []                                             # list of components
         prev = 0
         for match in matches:
@@ -46,11 +78,10 @@ class FormulaCell():
 
         result.append(input[prev:])                             # append the content after the last match
         resultString = ''.join(result)                          # convert the result list into a string
-        print('---->>  '+resultString)
         return resultString
 
     def __str__(self):
-        self.updateValue()
+        #self.updateValue()
         return str(self.value)
 
 
@@ -62,9 +93,11 @@ class Sheet(object):
         # fill the sheet with zero numbercells
         for row in range(self.rows):
             for col in range(self.cols):
-                self.updateValue(row, col, "0")
+                self.modifyValue(row, col, "0")
 
-    def updateValue(self, row, col, newValue):
+
+
+    def modifyValue(self, row, col, newValue):
         if newValue.isdigit():
             cellObject = NumberCell(newValue)
         else:
@@ -99,6 +132,18 @@ class Sheet(object):
         col = self.colNameToInt(letters)
         cell = self.matrix.getElementAt(row, col)
         return cell.value
+
+    def lookupCell(self, x):
+
+        p = re.compile('[A-Z]+')
+        matches = p.match(x)
+        to = matches.end()
+        letters = x[:to]
+        digits = x[to:]
+        row = int(digits) - 1  # for 0 based matrix index
+        col = self.colNameToInt(letters)
+        cell = self.matrix.getElementAt(row, col)
+        return cell
 
     def __str__(self):
         return self.matrix.__str__()
